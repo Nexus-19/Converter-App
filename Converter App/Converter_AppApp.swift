1086f1e6349dc314eb45696785542367f034b8db
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-06-18 14:40:58 +0530

    Initial Commit

---
+++ Converter App/Converter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Converter_AppApp.swift
+//  Converter App
+//
+//  Created by Soumyattam Dey on 18/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Converter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
